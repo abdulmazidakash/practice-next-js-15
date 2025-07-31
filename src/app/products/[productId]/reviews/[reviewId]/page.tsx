1dfb224c7b8f95adef6419492fa33c19d5fdaf31
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-07-31 11:09:02 +0600

    feat(route): add globally and route  not found page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,7 +1,12 @@
+import { notFound } from 'next/navigation';
 import React from 'react'
 
 export default async function ReviewDetails({params}: {params: Promise<{productId: string, reviewId: string}>}) {
 	const {productId, reviewId} = await params;
+
+	if(reviewId > '50'){
+		notFound()
+	}
   return (
 	<div>Product Id: {productId} | Review Id: {reviewId}</div>
   )
