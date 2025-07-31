23ec8ccfef06fb04f89954858b311d83fa066be5
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-07-31 11:26:02 +0600

    feat: add private  route lib folder

--- src/app/charts/line-chart.tsx
+++ src/app/charts/line-chart.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
 
-function LineChart(){
-	return <h2>Line Chart</h2>
-}
 
 export default function lineChart() {
   return (
-	<div>LineChart()</div>
+	<div>LineChart</div>
   )
 }
