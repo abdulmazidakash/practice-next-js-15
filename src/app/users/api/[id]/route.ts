31edd0861c21b759ab697cba040354a5032cb860
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-08-03 00:27:52 +0600

    feat: patch, delete and url query params add

--- src/app/users/api/[id]/route.ts
+++ src/app/users/api/[id]/route.ts
@@ -1,7 +1,24 @@
 import { users } from "@/data/users";
 
-export async function GET(request: Request, {params}: {params: {id: string}}){
+export async function GET(request: Request, {params}: {params: Promise<{id: string}>}){
 	const {id } = (await params);
 	const user = users.find(item => item.id === Number(id));
 	return Response.json(user)
+};
+
+export async function PATCH(request: Request, {params}: {params: Promise<{id: string}>}){
+	const {id} = (await params);
+	const userRequest = await request.json();
+	const userIndex = users.findIndex(item=> item.id == Number(id));
+	users[userIndex]['name'] = userRequest.name;
+	return Response.json(users[userIndex])
+};
+
+export async function DELETE(_request: Request, {params}:{params: Promise<{id: string}>}) {
+	const { id } = (await params);
+	const userIndex = users.findIndex(item => item.id == Number(id));
+	if(userIndex > 0){
+		users.splice(userIndex, 1)
+	};
+	return Response.json({message: 'user deleted successfully'})
 }
--- src/app/users/api/route.ts
+++ src/app/users/api/route.ts
@@ -1,9 +1,13 @@
 import { users } from "@/data/users"
+import { NextRequest } from "next/server";
 
 
-export async function GET(){
+export async function GET(request: NextRequest){
+	const searchParams = request.nextUrl.searchParams;
+	const userName = searchParams.get('username');
+	const filteredUsers = userName? users.find(item=> item.username == userName): users
 	// return new Response('Hello users route handler')
-	return  Response.json(users)
+	return  Response.json(filteredUsers)
 }
 
 export async function POST(request: Request){
