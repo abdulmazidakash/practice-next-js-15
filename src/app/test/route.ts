6375d1ace20b6838cf584fe613d833f3d870befd
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-08-03 23:02:00 +0600

    feat: content type and cookies set

--- src/app/test/route.ts
+++ src/app/test/route.ts
@@ -1,4 +1,18 @@
-export async function GET(){
-	return new Response('Hello next.js route handlers')
+import { headers } from "next/headers";
+import { type NextRequest } from "next/server";
+
+export async function GET(request: NextRequest){
+	const reqHeaders = new Headers(request.headers);
+	console.log(reqHeaders.get('abc'));
+
+	const userCookie = request.cookies.get('test');
+	console.log(userCookie);
+
+	return new Response('<h2>Hello next.js route handlers</h2>', {
+		headers: {
+			'content-type': 'text/html',
+			'Set-Cookie': 'test=12345',
+		}
+	})
 }
 
